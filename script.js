8f5a73cfbf325c18772d72aaaf243aec10d19323
Amandeep singh <amansingh765487@gmail.com>
2026-02-17 21:38:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	width:number;
+	heigth:number;
+	
+	constructor(width:number, heigth:number){
+		this.width = width 
+		this.heigth = heigth 
+	
+}
+rectangleINstance(){
+const rectangleArea = this.widht * this.heigth;
+	return rectangleArea
+}
+}
 
+class Square extends Rectangle{
+   constructor(widht,heigth){
+  super(widht,heigth);
+   }
+
+squareInstance(){
+const squareArea = this.widht * this.height
+	return squareArea
+}
+}
 class Square extends Animal {}
 
 // Do not change the code below this line
