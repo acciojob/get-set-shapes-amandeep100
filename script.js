baefab984e119a880b90601f7d03e660c079eefa
Amandeep singh <amansingh765487@gmail.com>
2026-02-17 21:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 //complete this code
-class Rectangle {
-	// width:number;
-	// heigth:number;
-	
-	constructor(width, heigth){
-		this.width = width 
-		this.heigth = heigth 
-	
-}
-rectangleINstance(){
-const rectangleArea = this.widht * this.heigth;
-	return rectangleArea
-}
+ class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  set width(value) {
+    this._width = value;
+  }
+
+  set height(value) {
+    this._height = value;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
 }
 
-class Square extends Rectangle{
-   constructor(side){
-  super(side,side);
-   }
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
 
-squareInstance(){
-const squareArea = this.widht * this.height
-	return squareArea
+  getPerimeter() {
+    return this.width * 4;
+  }
 }
-}
-
-
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
